d45adf32324d8ae2b034336bfc379aa76f240a8c
Syed Muhammad Hassan <hasan151008@gmail.com>
2026-02-26 00:14:56 +0500

    react project using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Samurai from "./Samurai"
+
+function App() {
+  return (
+    <>
+      <h1>Samurai</h1>
+      <Samurai />
+      <p>He is the best samurai</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Samurai.jsx
@@ -0,0 +1,7 @@
+function Samurai() {
+    return(
+        <h2>He kills his opponents with honor</h2>
+    )
+}
+
+export default Samurai;
