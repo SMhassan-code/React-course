729c9ca0af24dd9aafe90ce0ea787b3d6fb54f41
Syed Muhammad Hassan <hasan151008@gmail.com>
2026-02-26 00:14:28 +0500

    learning flow of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import Knight from "./Knight";
+
+function App() {
+  return (
+    <>
+      <h1>React website</h1>
+      <Knight />
+      <p>He is incredibly powerful</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Knight.js
@@ -0,0 +1,7 @@
+function Knight() {
+    return (
+        <h2>You are a honorable knight</h2>
+    )
+}
+
+export default Knight;
