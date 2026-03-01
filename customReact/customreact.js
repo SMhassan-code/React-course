1227780ae0ef129198a7309b5d1ea39c1232cc37
Syed Muhammad Hassan <hasan151008@gmail.com>
2026-03-01 22:56:57 +0500

    custom react

---
+++ customReact/customreact.js
@@ -0,0 +1,27 @@
+const root = document.getElementById('root')
+
+const reactElement = {
+    type: "a",
+    props: {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: "Click to visit google"
+}
+
+function customReact(reactElement, conatiner) {
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+
+    // domElement.setAttribute("href", reactElement.props.href)
+    // domElement.setAttribute("target", reactElement.props.target)
+
+    for (const prop in reactElement.props) {
+        if (prop === "children") continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+
+    conatiner.appendChild(domElement)
+}
+
+customReact(reactElement, root)
