59cca0326cb998a839f7ef77fd1acb334d81a7b0
Syed Muhammad Hassan <hasan151008@gmail.com>
2026-03-19 17:31:06 +0500

    todo app project with context api

---
+++ 10todoContextLocal/src/contexts/todoContext.js
@@ -0,0 +1,21 @@
+import { useContext, createContext } from "react";
+
+export const todoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {},
+})
+
+export const TodoProvider = todoContext.Provider
+
+export const useTodo = () => {
+    return useContext(todoContext)
+}
